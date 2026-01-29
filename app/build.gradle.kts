21058fd61bfd3edd4ae15f8b9c4619ca9df4e6c8
sfsakhawat999 <sfsakhawat999@gmail.com>
2026-01-29 17:51:49 +0600

    chore: update versionCode to 128 and versionName to 1.2.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "xyz.mpv.rex"
     minSdk = 26
     targetSdk = 36
-    versionCode = 127
-    versionName = "1.2.7"
+    versionCode = 128
+    versionName = "1.2.7.1"
 
     vectorDrawables {
       useSupportLibrary = true
